698d3107d390fea75c7dd7f90d18e39a3bfc9878
Sara886a <43111320+Sara886a@users.noreply.github.com>
2019-10-07 14:58:59 +0200

    counter as in processing

--- card_test/venv/Scripts/code/BLOB.py
+++ card_test/venv/Scripts/code/BLOB.py
@@ -5,41 +5,43 @@ def main():
     img = Image.open('Images/ace2.JPG')
     size = width, height = img.size
     img2 = binary(img)
-    counter = Counter(img2)
-    img3 = detectBlobs(img2,counter)
+    img3 = detectBlobs(img2)
 
     img3.show()
     del img, img2, img3
 
-def detectBlobs(img, counter):
+def detectBlobs(img):
+    pixels = list(img.getdata())
     size = width, height = img.size
     pos = 0
+    counter = Counter()
 
-    for R, G, B in counter.pixels:
+    for R, G, B in pixels:
         if (R == 255):
-            grassFire(pos, counter.pixels, width, height, counter)
+            grassFire(pos, pixels, width, height, counter)
             if (counter.pixelCount > 400):
                 counter.blobCount += 1
             counter.pixelCount = 0
         pos += 1
-    img.putdata(counter.pixels)
+    img.putdata(pixels)
     print(counter.blobCount)
     return img
 
 
 def grassFire(pos, pixels, width, height, counter):
     counter.pixelCount += 1
-    counter.pixels[pos] = (200, 10, 150)
+    pixels[pos] = (200, 10, 150)
 
-    if (pos + 1 < width and counter.pixels[pos + 1] == (255, 255, 255)):
-        grassFire(pos + 1, counter.pixels, width, height, counter)
-    if (pos + width < height and counter.pixels[pos + width] == (255, 255, 255)):
-        grassFire(pos + width, counter.pixels, width, height, counter)
-    if (pos - 1 > 0 and counter.pixels[pos - 1] == (255, 255, 255)):
-        grassFire(pos - 1, counter.pixels, width, height, counter)
-    if (pos - width > 0 and counter.pixels[pos - width] == (255, 255, 255)):
-        grassFire(pos - width, counter.pixels, width, height, counter)
+    if (pos + 1 < width and pixels[pos + 1] == (255, 255, 255)):
+        grassFire(pos + 1, pixels, width, height, counter)
+    if (pos + width < height and pixels[pos + width] == (255, 255, 255)):
+        grassFire(pos + width, pixels, width, height, counter)
+    if (pos - 1 > 0 and pixels[pos - 1] == (255, 255, 255)):
+        grassFire(pos - 1, pixels, width, height, counter)
+    if (pos - width > 0 and pixels[pos - width] == (255, 255, 255)):
+        grassFire(pos - width, pixels, width, height, counter)
 
+    return pixels
 
 
 def randomColor():
@@ -64,7 +66,6 @@ def binary(img):
 class Counter:
     pixelCount = 0
     blobCount = 0
-    pixels = list(img.getdata())
 
 
 if __name__ == '__main__':
