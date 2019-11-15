005ae6564d9ba61a6406d6ecbc446420002415d0
Dovile <43847677+dovii@users.noreply.github.com>
2019-11-15 15:36:03 +0100

    added new templates
    cleaning up, adding our own card templates

--- openCV_version/venv/Scripts/code/compare_images.py
+++ openCV_version/venv/Scripts/code/compare_images.py
@@ -1,26 +1,28 @@
 import cv2
 import numpy as np
 
-original = cv2.imread('Images/Nine.jpg')
-template = cv2.imread('Images/Jack.jpg')
+original = cv2.imread('Images/Templates/K.png')
+template = cv2.imread('Images/Templates/A.png')
 
-image1 = original.shape
+image1 = original.shape #gives information about size and channels of the images (3 for b g r)
 image2 = template.shape
 
 print(image1)
 print(image2)
 
-if original.shape == template.shape:
+if original.shape == template.shape: #gives info if the channels and size matches
     print("same size and channels")
-    difference = cv2.subtract(original, template)
-    b, g, r = cv2.split(difference) #separate the r b g channels
 
-    print(cv2.countNonZero(b))
+    difference = cv2.subtract(original, template) #subtracting template pixels from original image's pixels
+
+    b, g, r = cv2.split(difference) #separate the b g r channels
+
+    #print(cv2.countNonZero(b)) the difference in blue channel
 
     cv2.imshow("difference", difference)
 
 
-
+    #self-defined  countnonzero bgr treshold values. Should be adjusted after more testing
     if cv2.countNonZero(b) <= 100 and cv2.countNonZero(g) <= 100 and cv2.countNonZero(r) <= 100:
         print("equal")
 
