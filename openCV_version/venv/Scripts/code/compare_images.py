4cf6c501c8a48565db9d13ab2253ed4a65af2bbd
Dovile <43847677+dovii@users.noreply.github.com>
2019-11-20 13:59:20 +0100

    cleaning up
    deleting unnecessary pictures that were used for testing stuff. Deleted homography script as we didn't use it

--- openCV_version/venv/Scripts/code/compare_images.py
+++ openCV_version/venv/Scripts/code/compare_images.py
@@ -1,7 +1,7 @@
 import cv2
 
 original = cv2.imread('Images/Templates/K.png')
-template = cv2.imread('Images/Templates/A.png')
+template = cv2.imread('Images/Templates/K.png')
 
 image1 = original.shape #gives information about size and channels of the images (3 for b g r). Optional.
 image2 = template.shape
@@ -17,10 +17,9 @@ if original.shape == template.shape: #checks if the size and amount of channels 
 
     b, g, r = cv2.split(difference) #split an image into three different intensity arrays for each color channel (b g r)
 
-    #print(cv2.countNonZero(b)) the difference in blue channel
+   # print(cv2.countNonZero(b)) #the difference in blue channel
 
     cv2.imshow("subtracted images", difference)
-
 
     #countNonZero - counts the empty spots in the array of pixels (determines white pixels)
     #less white pixels means pictures are more likely to be equal
