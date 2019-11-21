ffcf5617148348ea6a4153b8153f4913202fe3bf
Jokubas126 <jokubas126@users.noreply.github.com>
2019-11-21 15:26:19 +0100

    Revert "Hand card sending to the server done"
    This reverts commit 4e747b56685f3534d88539c3dfccf2b374cbb461.

--- android_application/app/src/main/java/com/example/p3_android_application/ActivitySpeedometer.java
+++ android_application/app/src/main/java/com/example/p3_android_application/ActivitySpeedometer.java
@@ -1,9 +1,7 @@
 package com.example.p3_android_application;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class ActivitySpeedometer extends AppCompatActivity {
@@ -12,11 +10,5 @@ public class ActivitySpeedometer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            getSupportActionBar().hide();
-        } catch (NullPointerException e){
-            System.out.println("No action bar found to hide");
-        }
-        Intent previousActivity = getIntent(); // use for taking the info to here
     }
 }
