d334edf73493a929fc98e4fb7b0c9d9a3562f93f
Dovile <43847677+dovii@users.noreply.github.com>
2019-11-22 09:31:36 +0100

    created array of template images
    template images are compared to capture image until the right template is found

--- openCV_version/venv/Scripts/code/compare_images.py
+++ openCV_version/venv/Scripts/code/compare_images.py
@@ -1,33 +1,51 @@
 import cv2
+import glob
+
 
 original = cv2.imread('../Images/Templates/K.png')
-template = cv2.imread('../Images/Templates/K.png')
 
-image1 = original.shape #gives information about size and channels of the images (3 for b g r). Optional.
-image2 = template.shape
 
-print(image1)
-print(image2)
+all_templates = []
+card_name = []
 
-if original.shape == template.shape: #checks if the size and amount of channels in the images match. Optional.
-    print("same size and channels")
 
-    difference = cv2.subtract(original, template) #Calculates the pixel difference between original and template images
-    #subtracts black pixels as in these pictures they can be either black and white
+for i in glob.iglob("../Images/Templates/*"):
+    image = cv2.imread(i)
+    card_name.append(i)
+    all_templates.append(image)
 
-    b, g, r = cv2.split(difference) #split an image into three different intensity arrays for each color channel (b g r)
 
-   # print(cv2.countNonZero(b)) #the difference in blue channel
+for template, card_name in zip(all_templates, card_name): #zip allows to work with more than 1 array
 
-    cv2.imshow("subtracted images", difference)
+    image1 = original.shape #gives information about size and channels of the images (3 for b g r). Optional.
+    image2 = template.shape
 
-    #countNonZero - counts the empty spots in the array of pixels (determines white pixels)
-    #less white pixels means pictures are more likely to be equal
-    if cv2.countNonZero(b) <= 100 and cv2.countNonZero(g) <= 100 and cv2.countNonZero(r) <= 100:
-        print("equal")
+    print(image1)
+    print(image2)
 
-    else:
-        print("not equal")
+    if original.shape == template.shape: #checks if the size and amount of channels in the images match. Optional.
+        print("same size and channels")
+
+        difference = cv2.subtract(original, template) #Calculates the pixel difference between original and template images
+        #subtracts black pixels as in these pictures they can be either black and white
+
+        b, g, r = cv2.split(difference) #split an image into three different intensity arrays for each color channel (b g r)
+
+        print(cv2.countNonZero(b)) #the difference in blue channel
+
+        #cv2.imshow("subtracted images", difference)
+
+        #countNonZero - counts the empty spots in the array of pixels (determines white pixels)
+        #less white pixels means pictures are more likely to be equal
+        if cv2.countNonZero(b) <= 700: #only needs info from one channel
+            print("equal")
+            print("Matching card " + card_name)
+            break
+
+
+        else:
+            print("not equal")
+
 
 
 cv2.imshow("Original", original)
